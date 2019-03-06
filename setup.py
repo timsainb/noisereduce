031330a9b1408d459d69eb6d4d1b92fcffb2b051
timsainb <timsainb@gmail.com>
2019-03-06 14:11:58 -0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="noisereduce",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.1.2",
     description="Noise reduction using Spectral Gating in python",
     author="Tim Sainburg",
     license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timsainb/birdbrain",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+    ],
+    install_requires=["scipy", "matplotlib", "librosa", "numpy", "tqdm"],
 )
