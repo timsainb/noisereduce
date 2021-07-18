54b2776a7d6fb2545af8d9a27c349ed4d6f69639
timsainb <timsainb@gmail.com>
2021-07-18 13:26:56 -0700

    added tests

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="noisereduce",
     packages=find_packages(),
-    version="1.1.0",
+    version="2.0.0",
     description="Noise reduction using Spectral Gating in python",
     author="Tim Sainburg",
     license="MIT",
--- test_reduction.py
+++ test_reduction.py
@@ -85,3 +85,70 @@ def test_reduce_generated_noise_batches():
     return nr.reduce_noise(
         y=audio_clip_band_limited, sr=rate, stationary=False, chunk_size=30000
     )
+
+
+
+### Tests for V1
+
+
+def test_reduce_generated_noise():
+    # load data
+    wav_loc = "assets/fish.wav"
+    rate, data = wavfile.read(wav_loc)
+    data = int16_to_float32(data)
+    # add noise
+    noise_len = 2  # seconds
+    noise = (
+        band_limited_noise(
+            min_freq=2000, max_freq=12000, samples=len(data), samplerate=rate
+        )
+        * 10
+    )
+    noise_clip = noise[: rate * noise_len]
+    audio_clip_band_limited = data + noise
+    return nr.reduce_noise(
+        audio_clip=audio_clip_band_limited, noise_clip=noise_clip, verbose=True
+    )
+
+
+def test_reduce_cafe_noise():
+    # load data
+    wav_loc = "assets/fish.wav"
+    rate, data = wavfile.read(wav_loc)
+    data = int16_to_float32(data)
+    noise_loc = "assets/cafe_short.wav"
+    noise_rate, noise_data = wavfile.read(noise_loc)
+    noise_data = int16_to_float32(noise_data)
+    # add noise
+    snr = 2  # signal to noise ratio
+    noise_clip = noise_data / snr
+    audio_clip_cafe = data + noise_clip
+
+    # reduce noise
+    reduced_noise = nr.reduce_noise(
+        audio_clip=audio_clip_cafe, noise_clip=noise_clip, verbose=True
+    )
+    return float32_to_int16(reduced_noise)
+
+
+def test_reduce_cafe_noise_tf():
+    # load data
+    wav_loc = "assets/fish.wav"
+    rate, data = wavfile.read(wav_loc)
+    data = int16_to_float32(data)
+    noise_loc = "assets/cafe_short.wav"
+    noise_rate, noise_data = wavfile.read(noise_loc)
+    noise_data = int16_to_float32(noise_data)
+    # add noise
+    snr = 2  # signal to noise ratio
+    noise_clip = noise_data / snr
+    audio_clip_cafe = data + noise_clip
+
+    # reduce noise
+    reduced_noise = nr.reduce_noise(
+        audio_clip=audio_clip_cafe,
+        noise_clip=noise_clip,
+        use_tensorflow=True,
+        verbose=True,
+    )
+    return float32_to_int16(reduced_noise)
