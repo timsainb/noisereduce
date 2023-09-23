929fd4a9582a5f3cffe7ed2e026a620edca1ae7c
zorea <zoreasaf@gmail.com>
2023-09-23 21:43:11 +0300

    Fixed a bug related to the device setting when using torch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="noisereduce",
     packages=find_packages(),
-    version="3.0.0",
+    version="3.0.1",
     description="Noise reduction using Spectral Gating in Python",
     author="Tim Sainburg",
     license="MIT",
