312752364f1b512ab2383e2c6059a65318156cd9
zorea <zoreasaf@gmail.com>
2024-05-16 21:06:30 +0300

    replaced amplitude_to_db

--- noisereduce/spectralgate/utils.py
+++ noisereduce/spectralgate/utils.py
@@ -1,5 +1,4 @@
 import numpy as np
-from librosa.core import amplitude_to_db, db_to_amplitude
 
 
 def sigmoid(x, shift, mult):
@@ -9,15 +8,9 @@ def sigmoid(x, shift, mult):
     return 1 / (1 + np.exp(-(x + shift) * mult))
 
 
-def _amp_to_db(x):
+def _amp_to_db(x, top_db=80.0, eps=np.finfo(np.float64).eps):
     """
     Convert the input tensor from amplitude to decibel scale.
     """
-    return amplitude_to_db(x, ref=1.0, amin=1e-20, top_db=80.0)
-
-
-def _db_to_amp(x, ):
-    """
-    Convert the input tensor from decibel scale to amplitude.
-    """
-    return db_to_amplitude(x, ref=1.0)
+    x_db = 20 * np.log10(x.abs() + eps)
+    return np.maximum(x_db, (x_db.max(-1).values - top_db))
